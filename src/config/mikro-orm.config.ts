ec6b0750f072d83765220dae72e7479c001082f0
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2021-04-25 19:27:02 -0500

    update env variables

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -2,16 +2,26 @@ import { Options } from '@mikro-orm/core';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import config from '@root/config/config';
 import { findOneOrFailHandler } from '@root/utils';
+import fs from 'fs';
+import path from 'path';
+
+const connection = {
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync(path.join(process.cwd(), 'certs/ca.pem')).toString(),
+  },
+};
 
 export default {
   type: 'postgresql',
-  clientUrl: config.clientUrl,
+  dbName: config.dbName,
+  host: config.dbHost,
+  port: config.dbPort,
+  user: config.dbUser,
+  password: config.dbPassword,
   entities: ['./dist/**/entities/*'],
   entitiesTs: ['./src/**/entities/*'],
   findOneOrFailHandler,
-  driverOptions: {
-    connection: {
-      ssl: config.isProduction ? { rejectUnauthorized: false } : undefined,
-    },
-  },
+  driverOptions: config.isProduction ? { connection } : undefined,
+  debug: !config.isProduction,
 } as Options<PostgreSqlDriver>;
--- src/config/mikro-orm.migrations.config.ts
+++ src/config/mikro-orm.migrations.config.ts
@@ -1,18 +1,27 @@
 import { Options } from '@mikro-orm/core';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import config from '@root/config/config';
+import fs from 'fs';
+import path from 'path';
+
+const connection = {
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync(path.join(process.cwd(), 'certs/ca.pem')).toString(),
+  },
+};
 
 export default {
   type: 'postgresql',
-  clientUrl: config.migrationUrl,
+  dbName: config.dbName,
+  host: config.dbHost,
+  port: config.dbPort,
+  user: config.dbUser,
+  password: config.dbPassword,
   entities: ['./dist/**/entities/*'],
   entitiesTs: ['./src/**/entities/*'],
   migrations: {
     path: './src/__migrations__',
   },
-  driverOptions: {
-    connection: {
-      ssl: config.isProduction ? { rejectUnauthorized: false } : undefined,
-    },
-  },
+  driverOptions: config.isProduction ? { connection } : undefined,
 } as Options<PostgreSqlDriver>;
