1971344b105d76493ae494a16a7a5903a48e735c
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2021-04-27 18:23:36 -0500

    create weapon db tables

--- src/content-manager/weapons/entities/AmmoEntity.ts
+++ src/content-manager/weapons/entities/AmmoEntity.ts
@@ -1,5 +1,6 @@
 import { BaseEntity, Collection, EntitySchema } from '@mikro-orm/core';
 import { generateId } from '@root/utils';
+import { WeaponModeEntity } from '@content-manager/weapons/entities/WeaponModeEntity';
 
 export class AmmoEntity extends BaseEntity<AmmoEntity, 'id'> {
   id!: string;
@@ -7,6 +8,7 @@ export class AmmoEntity extends BaseEntity<AmmoEntity, 'id'> {
   link?: string | null;
   combinedAmmo = new Collection<AmmoEntity>(this);
   parentAmmo = new Collection<AmmoEntity>(this);
+  weaponModes = new Collection<WeaponModeEntity>(this);
 }
 
 export const ammoSchema = new EntitySchema({
@@ -27,6 +29,12 @@ export const ammoSchema = new EntitySchema({
       reference: 'm:n',
       entity: () => AmmoEntity,
       mappedBy: 'combinedAmmo',
+      hidden: true,
+    },
+    weaponModes: {
+      reference: 'm:n',
+      entity: () => WeaponModeEntity,
+      mappedBy: 'ammo',
       hidden: true,
     },
   },
